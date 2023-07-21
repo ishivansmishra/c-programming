9713e032cb96d47d6dc2e9d5134bd7a5bac696d1
Shivansh Mishra <131871626+ishivansmishra@users.noreply.github.com>
2023-07-21 15:50:46 +0530

    Add files via upload

---
+++ HELLO.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+//find the product of two value a and b.
+int main () {
+    int a;
+    printf("enter the value of a : ");
+    scanf("%d",&a);
+    int b;
+    printf("enter the value of b: ");
+    scanf("%d",&b);
+    int x = a*b;
+    printf("product is : %d",x);
+    return 0;
+}
